d0d3931bce2ecf74ce2d5a97f5c8271b537391c4
Edula1234 <130446457+Edula1234@users.noreply.github.com>
2023-05-05 21:12:58 +0530

    Create Armstrong number.c

---
+++ Armstrong number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n,r,sum= 0,temp;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp=n;
+    while(n>0)
+    {
+    r=n%10;
+    sum=(r*r*r);
+    n=n/10;
+    }
+    if (temp==sum){
+        printf("%d is an Armstrong number.", num);
+    }  
+    else
+     {
+        printf("%d is not an Armstrong number.", num);
+     }
+
+    return 0;
+}
