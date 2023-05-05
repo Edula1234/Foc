46be9fa178bd8145a3afda9c6ad988495529415b
Edula1234 <130446457+Edula1234@users.noreply.github.com>
2023-05-05 21:42:52 +0530

    Create square root.c

---
+++ square root.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float x;
+prints("enter value of x");
+scanf("%d",&x);
+prints("square root of %6.2f is %6.2f",x,sqrt(x));
+getch();
+}
